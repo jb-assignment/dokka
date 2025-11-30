3700a2002e63905238cc50372aba84e36c957282
radoslaw.panuszewski <radoslaw.panuszewski@allegro.com>
2025-11-30 20:42:32 +0100

    Add TeamCity config

---
+++ .teamcity/settings.kts
@@ -0,0 +1,34 @@
+import dev.panuszewski.distributedkotest.teamcity.DistributedTests
+import jetbrains.buildServer.configs.kotlin.DslContext
+import jetbrains.buildServer.configs.kotlin.project
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.ui.add
+import jetbrains.buildServer.configs.kotlin.version
+
+version = "2025.07"
+
+project {
+    params {
+        param("teamcity.buildQueue.restartBuildAttempts", "0")
+        param("teamcity.agent.dead.threshold.secs", "3600")
+        param("teamcity.agent.inactive.threshold.secs", "3600")
+    }
+
+    val jvmTests = DistributedTests(testTask = "check", numberOfBatches = 5) {
+        vcs {
+            root(DslContext.settingsRoot)
+        }
+
+        triggers {
+            vcs { }
+        }
+
+        requirements {
+            add {
+                matches("teamcity.agent.jvm.os.family", "Linux")
+            }
+        }
+    }
+
+    buildType(jvmTests)
+}
